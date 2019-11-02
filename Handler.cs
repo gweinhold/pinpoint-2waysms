abf0ca153128786aa1472fdd5d41ec0c9d7e279d
Geoff Weinhold <gweinhold@gmail.com>
2019-11-02 13:59:50 -0700

    dynamodb

--- Handler.cs
+++ Handler.cs
@@ -1,4 +1,6 @@
 using Amazon;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DocumentModel;
 using Amazon.Lambda.Core;
 using Amazon.Lambda.SNSEvents;
 using Amazon.Pinpoint;
@@ -8,17 +10,19 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
-[assembly:LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
 
 namespace AwsDotnetCsharp
 {
     public class Handler
     {
         private readonly AmazonPinpointClient client;
+        private readonly AmazonDynamoDBClient dBClient;
 
         private readonly string region;
         private readonly string originationNumber;
         private readonly string projectId;
+        private readonly string tableName;
         private static readonly string messageType = "TRANSACTIONAL";
         private static readonly string registeredKeyword = "myKeyword";
         private static readonly string senderId = "mySenderId";
@@ -29,6 +33,8 @@ namespace AwsDotnetCsharp
             originationNumber = Environment.GetEnvironmentVariable("originationNumber");
             projectId = Environment.GetEnvironmentVariable("projectId");
             client = new AmazonPinpointClient(RegionEndpoint.GetBySystemName(region));
+            dBClient = new AmazonDynamoDBClient();
+            tableName = Environment.GetEnvironmentVariable("dynamodbTable");
         }
 
         public async Task<Response> ProcessSms(SNSEvent snsEvent)
@@ -70,7 +76,8 @@ namespace AwsDotnetCsharp
                 {
                     case "REPORT":
                         Console.WriteLine("Getting Report");
-                        smsResponse.Body = "Here's your report";
+                        string reportId = incomingBody.Substring(6, incomingBody.Length - 6);
+                        smsResponse.Body = GetReport(reportId).Result;
                         break;
                     case "ECHO":
                         Console.WriteLine("Echo");
@@ -78,7 +85,7 @@ namespace AwsDotnetCsharp
                         break;
                     default:
                         Console.WriteLine("Default");
-                        smsResponse.Body = "Thanks for your message";
+                        smsResponse.Body = "Thanks for your message -1";
                         break;
                 }
 
@@ -86,6 +93,30 @@ namespace AwsDotnetCsharp
             }
 
             return null;
+        }
+
+        private async Task<string> GetReport(string reportId)
+        {
+            Console.WriteLine($"Getting report for {reportId}.");
+
+            var table = Table.LoadTable(dBClient, tableName);
+            var document = await table.GetItemAsync(reportId);
+            Console.WriteLine(document["Id"].AsString());
+
+            // var request = new GetItemRequest
+            // {
+            //     TableName = tableName,
+            //     Key = new Dictionary<string, AttributeValue>()
+            //     {
+            //         { "Id", new AttributeValue { S = reportId } }
+            //     }
+            // };
+
+            //            var response = await dBClient.GetItemAsync(request);
+            var message = $"Report: {document["Id"].AsString()} Created by {document["CreatedBy"].AsString()} on {document["CreateDateTime"].AsString()} Data: {document["Body"].AsString()}";
+
+            Console.WriteLine(message);
+            return message;
         }
 
         private async Task SendReplyToSender(SMSMessage message, string destinationNumber)
